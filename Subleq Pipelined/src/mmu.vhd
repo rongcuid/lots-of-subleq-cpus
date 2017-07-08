3fe71c165bb4e4c68d32b6ccaafae8497b243136
Rongcui Dong <rongcuid@outlook.com>
2017-07-07 18:48:54 -0700

    GHDL Bug Encountered

--- Subleq Pipelined/src/mmu.vhd
+++ Subleq Pipelined/src/mmu.vhd
@@ -44,10 +44,10 @@ entity MMU is
     resetb : in std_logic;
     we : in std_logic;
     en : in std_logic;
-    addr : in std_logic_vector(31 downto 3);
+    addr : in std_logic_vector(28 downto 0);
     be7n, be6n, be5n, be4n, be3n, be2n, be1n, be0n : in std_logic;
-    di : in std_logic_vector(WIDTH-1 downto 0);
-    do : out std_logic_vector(WIDTH-1 downto 0);
+    di : in std_logic_vector(63 downto 0);
+    do : out std_logic_vector(63 downto 0)
     );
 end MMU;
 
@@ -65,81 +65,98 @@ architecture behavior of MMU is
   signal di_ram0, di_ram1, di_ram2, di_ram3, di_ram4, di_ram5, di_ram6, di_ram7 : std_logic_vector(7 downto 0);
   -- Memory Bank Data Out
   signal do_ram0, do_ram1, do_ram2, do_ram3, do_ram4, do_ram5, do_ram6, do_ram7 : std_logic_vector(7 downto 0);
+
+  component BRAM_SP 
+    generic ( WIDTH : integer := 32;
+              DEPTH : integer := 1024;
+              DEPTH_LOG : integer := 10
+              );
+    port (
+      clk : in std_logic;
+      we : in std_logic;
+      en : in std_logic;
+      addr : in std_logic_vector(DEPTH_LOG-1 downto 0);
+      di : in std_logic_vector(WIDTH-1 downto 0);
+      do : out std_logic_vector(WIDTH-1 downto 0)
+      );
+  end component BRAM_SP;
+
 begin
   -- The main memory bank.
   RAM0 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram0, en => en_ram0,
-    addr => addr(11 downto 2),
-    di => di_ram0, do => do_ram0
-    );
+      clk => clk,
+      we => we_ram0, en => en_ram0,
+      addr => addr(11 downto 2),
+      di => di_ram0, do => do_ram0
+      );
   RAM1 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram1, en => en_ram1,
-    addr => addr(11 downto 2),
-    di => di_ram1, do => do_ram1
-    );
+      clk => clk,
+      we => we_ram1, en => en_ram1,
+      addr => addr(11 downto 2),
+      di => di_ram1, do => do_ram1
+      );
   RAM2 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram2, en => en_ram2,
-    addr => addr(11 downto 2),
-    di => di_ram2, do => do_ram2
-    );
+      clk => clk,
+      we => we_ram2, en => en_ram2,
+      addr => addr(11 downto 2),
+      di => di_ram2, do => do_ram2
+      );
   RAM3 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram3, en => en_ram3,
-    addr => addr(11 downto 2),
-    di => di_ram3, do => do_ram3
-    );
+      clk => clk,
+      we => we_ram3, en => en_ram3,
+      addr => addr(11 downto 2),
+      di => di_ram3, do => do_ram3
+      );
   RAM4 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram4, en => en_ram4,
-    addr => addr(11 downto 2),
-    di => di_ram4, do => do_ram4
-    );
+      clk => clk,
+      we => we_ram4, en => en_ram4,
+      addr => addr(11 downto 2),
+      di => di_ram4, do => do_ram4
+      );
   RAM5 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram5, en => en_ram5,
-    addr => addr(11 downto 2),
-    di => di_ram5, do => do_ram5
-    );
+      clk => clk,
+      we => we_ram5, en => en_ram5,
+      addr => addr(11 downto 2),
+      di => di_ram5, do => do_ram5
+      );
   RAM6 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram6, en => en_ram6,
-    addr => addr(11 downto 2),
-    di => di_ram6, do => do_ram6
-    );
+      clk => clk,
+      we => we_ram6, en => en_ram6,
+      addr => addr(11 downto 2),
+      di => di_ram6, do => do_ram6
+      );
   RAM7 : BRAM_SP
     generic map (WIDTH => 8, DEPTH => DEPTH_BANK)
     port map(
-    clk => clk,
-    we => we_ram7, en => en_ram7,
-    addr => addr(11 downto 2),
-    di => di_ram7, do => do_ram7
-    );
+      clk => clk,
+      we => we_ram7, en => en_ram7,
+      addr => addr(11 downto 2),
+      di => di_ram7, do => do_ram7
+      );
 
   -- The main mux which selects devices
   selector : process (we, en, addr, di, be7n, be6n, be5n, be4n,
                       be3n, be2n, be1n, be0n)
+  begin
     -- Write and Enable are all inactive by default
-    we_ram0 <= 0; en_ram0 <= 0; we_ram1 <= 0; en_ram1 <= 0;
-    we_ram2 <= 0; en_ram2 <= 0; we_ram3 <= 0; en_ram3 <= 0;
-    we_ram4 <= 0; en_ram4 <= 0; we_ram5 <= 0; en_ram5 <= 0;
-    we_ram6 <= 0; en_ram6 <= 0; we_ram7 <= 0; en_ram7 <= 0;
+    we_ram0 <= '0'; en_ram0 <= '0'; we_ram1 <= '0'; en_ram1 <= '0';
+    we_ram2 <= '0'; en_ram2 <= '0'; we_ram3 <= '0'; en_ram3 <= '0';
+    we_ram4 <= '0'; en_ram4 <= '0'; we_ram5 <= '0'; en_ram5 <= '0';
+    we_ram6 <= '0'; en_ram6 <= '0'; we_ram7 <= '0'; en_ram7 <= '0';
     -- By default, MMU generates invalid data
     do <= (others => '-');
     if (addr(31) = '0') then
@@ -184,7 +201,7 @@ begin
         en_ram7 <= en;
       end if;
     else
-      -- If not main memory. This is not implemented in this project
+    -- If not main memory. This is not implemented in this project
     end if; 
   end process selector;
 
--- Subleq Pipelined/test/simulation/test_mmu_tb.vhd
+++ Subleq Pipelined/test/simulation/test_mmu_tb.vhd
@@ -43,27 +43,27 @@ architecture behavioral of test_mmu_tb is
       en : in std_logic;
       addr : in std_logic_vector(31 downto 3);
       be7n, be6n, be5n, be4n, be3n, be2n, be1n, be0n : in std_logic;
-      di : in std_logic_vector(WIDTH-1 downto 0);
-      do : out std_logic_vector(WIDTH-1 downto 0);
+      di : in std_logic_vector(63 downto 0);
+      do : out std_logic_vector(63 downto 0)
       );
   end component MMU;
 
   -- Signals
-  clk_tb, resetb_tb, we, en : std_logic;
-  be7n_tb, be6n_tb, be5n_tb, be4n_tb : std_logic;
-  be3n_tb, be2n_tb, be1n_tb, be0n_tb : std_logic;
-  addr_tb : std_logic_vector(31 downto 0);
-  di_tb, do_tb : std_logic_vector(63 downto 0);
+  signal clk_tb, resetb_tb, we_tb, en_tb : std_logic;
+  signal be7n_tb, be6n_tb, be5n_tb, be4n_tb : std_logic;
+  signal be3n_tb, be2n_tb, be1n_tb, be0n_tb : std_logic;
+  signal addr_tb : std_logic_vector(31 downto 0);
+  signal di_tb, do_tb : std_logic_vector(63 downto 0);
   -- Test procedures
   procedure reset (
-    signal resetb_tb : out std_logic;
+    signal resetb : out std_logic
     ) is
   begin
     wait until rising_edge(clk_tb);
-    resetb_tb <= '0';
+    resetb <= '0';
     wait until rising_edge(clk_tb);
     wait until rising_edge(clk_tb);
-    resetb_tb <= '1';
+    resetb <= '1';
 
   end procedure reset;
   procedure test_template(
@@ -72,12 +72,12 @@ architecture behavioral of test_mmu_tb is
     signal be3n, be2n, be1n, be0n : out std_logic;
     signal addr : out std_logic_vector(31 downto 0);
     signal di : out std_logic_vector(63 downto 0);
-    signal do : out std_logic_vector(63 downto 0);
+    signal do : out std_logic_vector(63 downto 0)
     ) is
     begin
   end procedure test_template;
   procedure test_1(
-    signal we, en : out std_logic;
+    signal resetb, we, en : out std_logic;
     signal be7n, be6n, be5n, be4n : out std_logic;
     signal be3n, be2n, be1n, be0n : out std_logic;
     signal addr : out std_logic_vector(31 downto 0);
@@ -86,24 +86,28 @@ architecture behavioral of test_mmu_tb is
     ) is
     variable addr_tmp : integer;
     variable addr_line, data_line : line;
+    constant data_unsigned : unsigned(63 downto 0) :=
+      X"DEADBEEFBAADC0DE";
+      
   begin
-    reset(resetb_tb);
+    we <= '0'; en <= '0';
+    be7n <= '0'; be6n <= '0'; be5n <= '0'; be4n <= '0';
+    be3n <= '0'; be2n <= '0'; be1n <= '0'; be0n <= '0';
+    reset(resetb);
     --wait until rising_edge(resetb_tb);
     --wait until rising_edge(clk_tb);
     for i in 0 to 15 loop
       -- Write 0xDEADBEEFBAADC0DE to address 0x0000000, 0x00000004, ...
       addr_tmp := i * 8;
       addr <= std_logic_vector(to_unsigned(addr_tmp, 32));
-      di <= std_logic_vector(
-        to_unsigned(16#DEADBEEFBAADC0DE#, 64)
-        );
+      di <= std_logic_vector(data_unsigned);
       -- Display some info
-      write(addr_line, addr);
-      write(data_line, do);
+      write(addr_line, addr_tb);
+      write(data_line, do_tb);
       report "Test 1 Iter " & integer'image(i) &
         ": Addr in = " & integer'image(addr_tmp) &
-        "; Prev_Addr = " & addr_line &
-        "; Prev_Data = " & data_line severity note;
+        "; Prev_Addr = " & addr_line.all &
+        "; Prev_Data = " & data_line.all severity note;
       -- Wait a clock
       wait until rising_edge(clk_tb);
     end loop;
@@ -113,7 +117,7 @@ begin
   UUT : MMU port map(
     clk => clk_tb, resetb => resetb_tb,
     we => we_tb, en => en_tb,
-    addr => addr_tb,
+    addr => addr_tb(31 downto 3),
     be7n=>be7n_tb, be6n=>be6n_tb,
     be5n=>be5n_tb, be4n=>be4n_tb,
     be3n=>be3n_tb, be2n=>be2n_tb,
@@ -125,7 +129,7 @@ begin
 
   stimulus : process
   begin
-    test_1(we_tb, en_tb,
+    test_1(resetb_tb, we_tb, en_tb,
            be7n_tb, be6n_tb, be5n_tb, be4n_tb,
            be3n_tb, be2n_tb, be1n_tb, be0n_tb,
            addr_tb, di_tb, do_tb
